036ee21ea28cecf4039c288fdc308d9be8d2102e
NG-Joseph <jcool4liv@gmail.com>
2020-10-07 11:54:35 +0100

    Changes

--- src/client_dev/user-react-web-client/src/app.interfaces.tsx
+++ src/client_dev/user-react-web-client/src/app.interfaces.tsx
@@ -1,6 +1,5 @@
-/**
- * Abstract base type for entities
- */
+
+
 export interface IBaseAbstract {
     id?: number;
     dateCreated?: Date;
@@ -10,9 +9,7 @@ export interface IBaseAbstract {
     lastChangeInfo?: string;
     deletedBy?: string;
 }
-/**
- * CustomTheme type
- */
+
 export interface ICustomTheme extends IBaseAbstract{
     name?: string;
     description?: string;
@@ -20,35 +17,45 @@ export interface ICustomTheme extends IBaseAbstract{
     bulmaProperties?: {primaryColor: string, primaryBackground: string};
 }
 
-/**
- * Tenant type
- */
-export interface ITenant extends IBaseAbstract{
-    uuid?: string;
-    code?: string;
-    name?: string;
-    contactFirstName?: string;
-    contactLastName?: string;
-    contactTitle?: string;
-    address?: string;
-    email?: string;
-    defaultURLSlug?: string
-    customURLSlug?: string
-    dateOfRegistration?: Date
-    active?: boolean;
-    customTheme?: ICustomTheme;
-    [key: string]: any
+export interface IRole extends IBaseAbstract{
+    name: string;
+    description: string;  
+    
 }
 
-/**
- * State variable type
- */
+
+export interface IUser extends IBaseAbstract{
+     firstName?: string;
+     middleName?: string;
+     lastName?: string;
+     commonName?: string;
+     gender?: string;
+     dateOfBirth?: Date | string ;
+     isActive?: boolean;
+     primaryEmailAddress?: string;
+     isPrimaryEmailAddressVerified?: boolean;
+     passwordSalt?: string;
+     passwordHash?: string;
+     isPasswordChangeRequired?: boolean;
+     resetPasswordToken?: string;
+     resetPasswordExpiration?: Date;
+     primaryEmailVerificationToken: string;
+     otpEnabled: boolean;
+     otpSecret?: string;
+    // profile?: Profile;
+    // role?: Role;
+     customTheme?: ICustomTheme;
+     [key: string]: any
+    
+
+}
+
 export interface IState {
-    tenants?: ITenant[];
-    tenant?: ITenant | null; //This can be use for tenant to edit or tenant to view, depending on the function being carried out
-    onAddTenant: boolean;
-    onViewTenant: boolean;
-    onEditTenant: boolean;
+    users?: IUser[];
+    user?: IUser | null; //This can be use for User to edit or User to view, depending on the function being carried out
+    onAddUser: boolean;
+    onViewUser: boolean;
+    onEditUser: boolean;
     alert: {
         show: boolean,
         message: string,
@@ -56,26 +63,19 @@ export interface IState {
     }
 }
 
-/**
- * Action type for Reducer
- */
 export interface IAction {
     //Indicate possible reducer action types here as you identify them in your codes
-    type: 'FetchDataSuccess' | 'FetchDataFailure' | 'HandleOnAddTenant' 
-    | 'HandleCancelCreate' | 'BeforeCreateTenant' | 'CreateTenantSuccess' 
-    | 'CreateTenantFailure' | 'BeforeDeleteTenant' | 'DeleteTenantSuccess' 
-    | 'DeleteTenantFailure'| 'HandleEditTenant' | 'HandleCancelUpdate' 
-    | 'BeforeUpdateTenant' | 'UpdateTenantSuccess' | 'UpdateTenantFailure' 
-    | 'HandleCloseAlert' | 'HandleViewTenant' | 'HandleCloseViewTenant';
-    payload?: {tenants?: ITenant[], tenantsCount?: number, tenant?: ITenant, error?: Error, 
+    type: 'FetchDataSuccess' | 'FetchDataFailure' | 'HandleOnAddUser' 
+    | 'HandleCancelCreate' | 'BeforeCreateUser' | 'CreateUserSuccess' 
+    | 'CreateUserFailure' | 'BeforeDeleteUser' | 'DeleteUserSuccess' 
+    | 'DeleteUserFailure'| 'HandleEditUser' | 'HandleCancelUpdate' 
+    | 'BeforeUpdateUser' | 'UpdateUserSuccess' | 'UpdateUserFailure' 
+    | 'HandleCloseAlert' | 'HandleViewUser' | 'HandleCloseViewUser';
+    payload?: {users?: IUser[], usersCount?: number, user?: IUser, error?: Error, 
         id?: number | string}
 
 }
 
-/*
-The idea below is to provide room for specifying read
-https://github.com/typeorm/typeorm/blob/master/docs/find-options.md
-*/
 export interface IFindOptions {
     select?: string[];
     relations?: string[];
